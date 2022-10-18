9ac08224dc88c54a31c078ff2abfd76946d66e22
Kazumasa Utashiro <kaz@utashiro.com>
2022-10-18 12:17:11 +0900

    update BsdBib.pm

--- lib/Bombay/BsdBib.pm
+++ lib/Bombay/BsdBib.pm
@@ -7,10 +7,10 @@ use Data::Dumper;
 
 use Exporter 'import';
 our @EXPORT = qw(&bib_getmark &bib_makeref &bib_data &bib_ewb);
-our @EXPORT_OK = qw($bib_sort);
+our @EXPORT_OK = qw($bibsort);
 
 use Carp;
-use vars qw($myself $mydir $refdir $bibdir $bibindex %bibmark $bib_sort);
+use vars qw($myself $mydir $refdir $bibdir $bibindex %bibmark $bibsort);
 
 $myself = $0;
 $mydir = $myself;
@@ -20,26 +20,21 @@ $refdir = "$mydir/../Refs";
 $bibdir = "$refdir/EWB";
 $bibindex = "$refdir/00INDEX";
 
-#unless (opendir(BIBDIR, $bibdir)) {
-#    croak "$!: $bibdir";
-#}
-my %bibsort;
-open(INDEX, $bibindex) or croak "$!: $bibindex";
-while (<INDEX>) {
-    chomp;
-    my($mark, $keyword) = split /\t/, $_, 2;
-    $bibmark{$keyword} = $mark;
-    ($bibsort{$mark} = uc($mark)) =~ s/\W//g;
+my %sortkeys;
+if (open my $index, $bibindex) {
+    while (<$index>) {
+	chomp;
+	s/.*%%%//;
+	my($mark, $keyword) = split /\t/, $_, 2;
+	$bibmark{$keyword} = $mark;
+	($sortkeys{$mark} = uc($mark)) =~ s/\W//g;
+    }
+} else {
+    croak "$!: $bibindex";
 }
-close INDEX;
-#closedir BIBDIR;
 
-# works only on 5.6 or later
-#$bib_sort = sub ($$) {
-#	$bibsort{$_[0]} cmp $bibsort{$_[1]}
-#};
-$bib_sort = sub {
-    $bibsort{$main::a} cmp $bibsort{$main::b}
+$bibsort = sub {
+    $sortkeys{$::a} cmp $sortkeys{$::b}
 };
 
 sub bib_getmark {
