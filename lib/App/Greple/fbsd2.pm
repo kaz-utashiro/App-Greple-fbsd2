cebe774f8a1a27cf92ec4a0159904ad8255ed556
Kazumasa Utashiro <kaz@utashiro.com>
2022-10-23 23:45:03 +0900

    --ed2: 05 で5章を検索できるようにしてみた。

--- lib/App/Greple/fbsd2.pm
+++ lib/App/Greple/fbsd2.pm
@@ -584,8 +584,11 @@ option --colorcode  --need 1 --regioncolor \
 		    --le &part(mark)    --cm Y \
 		    --le &part(gap)     --cm N
 
-option --ed1 --chdir $ENV{FreeBSDbook}/1st_FreeBSD/daemon3 --glob c??.*/?.j
-option --ed2 --chdir $ENV{FreeBSDbook}/2nd_FreeBSD/ --glob c??.*/?.j
+option --ed1 --chdir $ENV{FreeBSDbook}/1st_FreeBSD/daemon3 --glob c$<shift>.*/?.j
+option --ed1 --ed1: ??
+
+option --ed2: --chdir $ENV{FreeBSDbook}/2nd_FreeBSD/ --glob c$<shift>.*/?.j
+option --ed2 --ed2: ??
 
 option --gloss1 --chdir $ENV{FreeBSDbook}/1st_FreeBSD/daemon3/c15.gloss --glob */*.j
 option --gloss2 --chdir $ENV{FreeBSDbook}/2nd_FreeBSD/c16.gloss --glob defs-*/*.j
